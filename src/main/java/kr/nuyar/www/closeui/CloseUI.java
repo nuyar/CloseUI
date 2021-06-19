f5ff9dadcc69ed894f96b1d55d2c3f81aa9b42f1
nuyar111 <qnfkdbs0222@naver.com>
2021-06-19 13:41:08 +0900

    0.1-SNAPSHOT
    spigot version 1.12.2-R0.1-SNAPSHOT

---
+++ src/main/java/kr/nuyar/www/closeui/CloseUI.java
@@ -0,0 +1,183 @@
+package kr.nuyar.www.closeui;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryView;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CloseUI extends JavaPlugin implements Listener {
+    public static CloseUI plugin;
+
+    private Inventory closingInventory;
+    private Map<String, Boolean> setting;
+
+    @Override
+    public void onEnable() {
+        CloseUI.plugin = this;
+
+        this.closingInventory = Bukkit.createInventory(null, 9, ChatColor.RED + "이 블럭/엔티티를 우클릭하지 말아주세요.");
+        this.setting = new HashMap<>();
+
+        this.getCommand("closeui").setExecutor(this);
+        this.getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onSettingEntity(PlayerInteractAtEntityEvent e) {
+        Player p = e.getPlayer();
+        if (!(this.setting.containsKey(p.getName()) && this.setting.get(p.getName())))
+            return;
+        this.setting.remove(p.getName());
+        e.setCancelled(true);
+
+        String entity = e.getRightClicked().getUniqueId().toString();
+
+        String world = p.getWorld().getName();
+        ConfigurationSection entitiesSection = this.getConfig().getConfigurationSection("entities");
+
+        if (entitiesSection == null)
+            entitiesSection = this.getConfig().createSection("entities");
+        List<String> entities = entitiesSection.getStringList(world);
+        if (entities == null)
+            entities = new ArrayList<>();
+
+        if (entities.contains(entity)) {
+            entities.remove(entity);
+            p.sendMessage(String.format("removed entity %s in %s", entity, world));
+        } else {
+            entities.add(entity);
+            p.sendMessage(String.format("added entity %s in %s", entity, world));
+        }
+
+        entitiesSection.set(world, entities);
+        this.saveConfig();
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onSettingBlock(PlayerInteractEvent e) {
+        Player p = e.getPlayer();
+        if (!(this.setting.containsKey(p.getName()) && this.setting.get(p.getName())))
+            return;
+        this.setting.remove(p.getName());
+        e.setCancelled(true);
+
+        Block block = e.getClickedBlock();
+
+        String world = p.getWorld().getName();
+        int x = block.getX();
+        int y = block.getY();
+        int z = block.getZ();
+        String location = String.format("%d %d %d", x, y, z);
+        ConfigurationSection blocksSection = this.getConfig().getConfigurationSection("blocks");
+
+        if (blocksSection == null)
+            blocksSection = this.getConfig().createSection("blocks");
+        List<String> blocks = blocksSection.getStringList(world);
+        if (blocks == null)
+            blocks = new ArrayList<>();
+
+        if (blocks.contains(location)) {
+            blocks.remove(location);
+            p.sendMessage(String.format("removed block %s in %s", location, world));
+        } else {
+            blocks.add(location);
+            p.sendMessage(String.format("added block %s in %s", location, world));
+        }
+
+        blocksSection.set(world, blocks);
+        this.saveConfig();
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onClickBlock(PlayerInteractEvent e) {
+        Player p = e.getPlayer();
+        Block block = e.getClickedBlock();
+
+        String world = p.getWorld().getName();
+        int x = block.getX();
+        int y = block.getY();
+        int z = block.getZ();
+        String location = String.format("%d %d %d", x, y, z);
+        ConfigurationSection blocksSection = this.getConfig().getConfigurationSection("blocks");
+
+        if (blocksSection == null)
+            return;
+        List<String> blocks = blocksSection.getStringList(world);
+        if (blocks == null)
+            return;
+
+        if (!blocks.contains(location))
+            return;
+
+        InventoryView inv = p.getOpenInventory();
+        if (!inv.getType().equals(InventoryType.CRAFTING))
+            return;
+
+        p.openInventory(closingInventory);
+        p.closeInventory();
+        //this.getServer().getScheduler().scheduleSyncDelayedTask(this, () -> player.closeInventory(), 1);
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onClickEntity(PlayerInteractAtEntityEvent e) {
+        Player p = e.getPlayer();
+        String entity = e.getRightClicked().getUniqueId().toString();
+
+        String world = p.getWorld().getName();
+        ConfigurationSection entitiesSection = this.getConfig().getConfigurationSection("entities");
+
+        if (entitiesSection == null)
+            return;
+        List<String> entities = entitiesSection.getStringList(world);
+        if (entities == null)
+            return;
+
+        if (!entities.contains(entity))
+            return;
+
+        InventoryView inv = p.getOpenInventory();
+        if (!inv.getType().equals(InventoryType.CRAFTING))
+            return;
+
+        p.openInventory(closingInventory);
+        p.closeInventory();
+        //this.getServer().getScheduler().scheduleSyncDelayedTask(this, () -> player.closeInventory(), 1);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!sender.isOp())
+            return true;
+        if (!(sender instanceof Player))
+            return true;
+        Player p = (Player) sender;
+
+        if (this.setting.containsKey(p.getName()) && this.setting.get(p.getName())) {
+            this.setting.remove(p.getName());
+            p.sendMessage("설정 모드를 종료합니다.");
+        } else {
+            this.setting.put(p.getName(), true);
+            p.sendMessage("엔티티 혹은 블럭을 우클릭해주세요.");
+        }
+
+        return true;
+    }
+}
